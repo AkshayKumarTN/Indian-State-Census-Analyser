db5c3b088c686acf52909d8670d42da43022c994
AkshayKumarTN <tnak369@gmail.com>
2021-07-30 16:43:53 +0530

    [Akshay] initial Commit

---
+++ IndianStateCensusAnalyser/CensusAnalyser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CensusAnalyserLive.DTO;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA,
+        }
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CsvAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+}
+© 2021 GitHub, Inc.
