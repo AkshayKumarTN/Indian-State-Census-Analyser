ef7fe775f3f5665a3e0af39d26d3d08dd7d72ee8
AkshayKumarTN <tnak369@gmail.com>
2021-07-30 18:34:27 +0530

    [Akshay] Added -> Loading State Code Data for Test Cases

--- IndianStateCensusAnalyser/CensusAnalyserException.cs
+++ IndianStateCensusAnalyser/CensusAnalyserException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace IndianStateCensusAnalyser
 {
-    class CensusAnalyserException : Exception
+    public class CensusAnalyserException : Exception
     {
         public enum ExceptionType
         {
